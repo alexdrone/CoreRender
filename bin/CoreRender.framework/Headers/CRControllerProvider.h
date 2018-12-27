e8ba9efbb04867a2dfc0f9102e0d0990e5e887ac
Alex Usbergo <alexusbergo@webrtc.org>
2018-12-27 13:45:42 +0100

    Distributable framework.

---
+++ bin/CoreRender.framework/Headers/CRControllerProvider.h
@@ -0,0 +1,11 @@
+//
+//  CRControllerProvider.h
+//  CoreRender
+//
+//  Created by Alex Usbergo on 12/4/18.
+//
+
+#ifndef CRControllerProvider_h
+#define CRControllerProvider_h
+
+#endif /* CRControllerProvider_h */
