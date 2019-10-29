c5baa490fa6a7dc6e585e3230c4e91fa8745f656
Alex Usbergo <alexusbergo@alexusbergo-imacpro.roam.corp.google.com>
2019-10-29 17:09:38 +0100

    format

--- Sources/CoreRenderObjC/CRNodeBuilder+Modifiers.mm
+++ Sources/CoreRenderObjC/CRNodeBuilder+Modifiers.mm
@@ -1,13 +1,13 @@
-#import "CRNodeBuilder+Modifiers.h"
 #import "CRController.h"
 #import "CRMacros.h"
+#import "CRNodeBuilder+Modifiers.h"
 #import "CRNodeLayoutSpec.h"
 #import "YGLayout.h"
 
 @implementation CRNodeBuilder (Modifiers)
 
 - (instancetype)padding:(UIEdgeInsets)padding {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.paddingTop) value:@(padding.top)];
     [spec set:CR_KEYPATH(spec.view, yoga.paddingBottom) value:@(padding.bottom)];
     [spec set:CR_KEYPATH(spec.view, yoga.paddingLeft) value:@(padding.left)];
@@ -16,7 +16,7 @@
 }
 
 - (instancetype)margin:(UIEdgeInsets)margin {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.marginTop) value:@(margin.top)];
     [spec set:CR_KEYPATH(spec.view, yoga.marginBottom) value:@(margin.bottom)];
     [spec set:CR_KEYPATH(spec.view, yoga.marginLeft) value:@(margin.left)];
@@ -25,7 +25,7 @@
 }
 
 - (instancetype)border:(UIEdgeInsets)border {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.borderTopWidth) value:@(border.top)];
     [spec set:CR_KEYPATH(spec.view, yoga.borderBottomWidth) value:@(border.bottom)];
     [spec set:CR_KEYPATH(spec.view, yoga.borderLeftWidth) value:@(border.left)];
@@ -34,134 +34,134 @@
 }
 
 - (instancetype)background:(UIColor *)color {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, backgroundColor) value:color];
   }];
 }
 
 - (instancetype)cornerRadius:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, clipsToBounds) value:@(YES)];
     [spec set:CR_KEYPATH(spec.view, layer.cornerRadius) value:@(value)];
   }];
 }
 
 - (instancetype)clipped:(BOOL)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, clipsToBounds) value:@(value)];
   }];
 }
 
 - (instancetype)hidden:(BOOL)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, hidden) value:@(value)];
   }];
 }
 
 - (instancetype)opacity:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, alpha) value:@(YES)];
   }];
 }
 
 - (instancetype)flexDirection:(YGFlexDirection)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.flexDirection) value:@(value)];
   }];
 }
 
 - (instancetype)justifyContent:(YGJustify)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.justifyContent) value:@(value)];
   }];
 }
 
 - (instancetype)alignContent:(YGAlign)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.alignContent) value:@(value)];
   }];
 }
 
 - (instancetype)alignItems:(YGAlign)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.alignItems) value:@(value)];
   }];
 }
 
 - (instancetype)alignSelf:(YGAlign)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.alignSelf) value:@(value)];
   }];
 }
 
 - (instancetype)position:(YGPositionType)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.position) value:@(value)];
   }];
 }
 
 - (instancetype)flexWrap:(YGWrap)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.flexWrap) value:@(value)];
   }];
 }
 
 - (instancetype)overflow:(YGOverflow)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.overflow) value:@(value)];
   }];
 }
 
 - (instancetype)flexGrow:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.flexGrow) value:@(value)];
   }];
 }
 
 - (instancetype)flexShrink:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.flexShrink) value:@(value)];
   }];
 }
 
 - (instancetype)flexBasis:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.flexBasis) value:@(value)];
   }];
 }
 
 - (instancetype)width:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.width) value:@(value)];
   }];
 }
 
 - (instancetype)height:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.height) value:@(value)];
   }];
 }
 
 - (instancetype)minWidth:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.minWidth) value:@(value)];
   }];
 }
 
 - (instancetype)minHeight:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.minHeight) value:@(value)];
   }];
 }
 
 - (instancetype)maxWidth:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.maxWidth) value:@(value)];
   }];
 }
 
 - (instancetype)maxHeight:(CGFloat)value {
-  return [self withLayoutSpec:^(CRNodeLayoutSpec * spec) {
+  return [self withLayoutSpec:^(CRNodeLayoutSpec *spec) {
     [spec set:CR_KEYPATH(spec.view, yoga.maxHeight) value:@(value)];
   }];
 }
